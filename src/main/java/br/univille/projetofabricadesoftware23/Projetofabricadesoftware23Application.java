14a78602f5b2853c9c92549d95ae7e55269cfa2b
RICARDO SCHNAIDER <ricardo.schnaider@univille.br>
2023-03-24 20:33:17 -0300

    first commit

---
+++ src/main/java/br/univille/projetofabricadesoftware23/Projetofabricadesoftware23Application.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabricadesoftware23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetofabricadesoftware23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetofabricadesoftware23Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projetofabricadesoftware23/Projetofabricadesoftware23ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabricadesoftware23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetofabricadesoftware23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
